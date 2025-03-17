ecfb859bc8bdad8e6241ca935a328795ad46fa48
jeanmfdias <jeanmfdias@gmail.com>
2025-03-17 15:52:32 -0300

    feat: init project

---
+++ src/test/java/jeanmfdias/auto/app/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package jeanmfdias.auto.app.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
