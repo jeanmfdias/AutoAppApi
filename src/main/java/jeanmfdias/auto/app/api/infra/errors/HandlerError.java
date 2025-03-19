b89427cd2258d6b5fe1e0adfd074fa8cc89060a7
jeanmfdias <jeanmfdias@gmail.com>
2025-03-18 21:08:52 -0300

    feat: refactory handlerError

--- src/main/java/jeanmfdias/auto/app/api/infra/errors/HandlerError.java
+++ src/main/java/jeanmfdias/auto/app/api/infra/errors/HandlerError.java
@@ -3,6 +3,7 @@ package jeanmfdias.auto.app.api.infra.errors;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -20,8 +21,13 @@ public class HandlerError {
 
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity<String> error404(EntityNotFoundException e) throws JsonProcessingException {
-        Map<String, String> json = Map.of("message", e.getMessage());
-        return ResponseEntity.status(404).body(this.objectMapper.writeValueAsString(json));
+        return this.responseMessageDefault(HttpStatus.NOT_FOUND.value(), e.getMessage());
+    }
+
+    private ResponseEntity<String> responseMessageDefault(int statusCode, String message) throws JsonProcessingException {
+        Map<String, String> json = Map.of("message", message);
+        return ResponseEntity.status(statusCode)
+                .body(this.objectMapper.writeValueAsString(json));
     }
 
 }
